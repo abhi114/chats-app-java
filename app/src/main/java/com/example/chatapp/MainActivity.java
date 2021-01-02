a2a1bbf9452fb3bb9de2bf06dd24140386a024f0
abhi114 <nabhi531@gmail.com>
2021-01-02 22:48:03 +0530

    login page

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager myViewPager; //view pager for scrolling between tabs
     private TabLayout myTabLayout; // for displaying the tabs
     private TabsAccessorAdapter myTabsAccessorAdapter; // adapter for viewing the tabs
+    private FirebaseUser currentUser; //Represents a user's profile information in your Firebase project's user database.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,5 +40,19 @@ public class MainActivity extends AppCompatActivity {
         myTabLayout = (TabLayout) findViewById(R.id.main_tabs);
         myTabLayout.setupWithViewPager(myViewPager);
 
+    }
+    // authentication
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //it means the user in not authenticated
+        if(currentUser == null){
+            SendUserToLoginActivity();
+        }
+    }
+
+    private void SendUserToLoginActivity() {
+        Intent loginIntent = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(loginIntent);
     }
 }

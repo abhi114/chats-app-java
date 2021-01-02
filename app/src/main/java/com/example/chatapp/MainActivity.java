aa39f1044541f9125ca9d30984b0abf3585f45b8
abhi114 <nabhi531@gmail.com>
2021-01-02 19:41:40 +0530

    initial commit

---
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Toolbar mToolbar;
+    private ViewPager myViewPager; //view pager for scrolling between tabs
+    private TabLayout myTabLayout; // for displaying the tabs
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        //setting the toolbar as the main app bar of the app
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("ChatApp");
+    }
+}
---
+++ app/src/main/java/com/example/chatapp/TabsAccessorAdapter.java
@@ -0,0 +1,51 @@
+package com.example.chatapp;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+//helps in switching between tabs
+//This version of the pager is best for use when there are a handful of typically more static fragments to be paged through, such as a set of tabs.
+public class TabsAccessorAdapter extends FragmentPagerAdapter {
+
+    //constructor fot the FragmentPagerAdapter
+    public TabsAccessorAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+
+    }
+
+
+    //switch statement for give the instance of the particular fragment
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position)
+        {
+            case 0:
+                ChatsFragment chatsFragment = new ChatsFragment();
+                return chatsFragment;
+
+            case 1:
+                groupsFragment GroupsFragment = new groupsFragment();
+                return GroupsFragment;
+
+            case 2:
+                ContactsFragment contactsFragment = new ContactsFragment();
+                return contactsFragment;
+
+            default:
+                return null;
+        }
+
+
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    //method to set title for these pages
+}

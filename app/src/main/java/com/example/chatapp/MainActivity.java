172b2522152658043126a91982dab5ce4a38e708
abhi114 <nabhi531@gmail.com>
2021-01-03 19:15:54 +0530

    settings activty

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -9,10 +9,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private TabsAccessorAdapter myTabsAccessorAdapter; // adapter for viewing the tabs
     private FirebaseUser currentUser; //Represents a user's profile information in your Firebase project's user database.
     private FirebaseAuth mAuth;
+    private DatabaseReference RootRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
+        RootRef = FirebaseDatabase.getInstance().getReference();
 
 
 
@@ -59,11 +67,48 @@ public class MainActivity extends AppCompatActivity {
         if(currentUser == null){
             SendUserToLoginActivity();
         }
+        // verify if user has give his username or not
+        else{
+            VerifyUserExistence();
+
+        }
+    }
+
+    private void VerifyUserExistence() {
+        //get the specific id of the user to check whether the id has the fields of username
+        String currentUserId = mAuth.getCurrentUser().getUid();
+        //now check using root ref
+        //Add a listener for changes in the data at this location. Each time time the data changes, your listener will be called with an immutable snapshot of the data.
+
+        RootRef.child(currentUserId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //if the user name is available in the database
+                if((dataSnapshot.child("name").exists())){
+                    Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
+
+                }else{
+                    SendUserToSettingsActivity();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
     }
 
     private void SendUserToLoginActivity() {
         Intent loginIntent = new Intent(MainActivity.this,LoginActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(loginIntent);
+        finish();
     }
 
     //now to add the options at the appbar
@@ -91,11 +136,21 @@ public class MainActivity extends AppCompatActivity {
             SendUserToLoginActivity();
         }
         if(item.getItemId() == R.id.main_settings_option){
+            SendUserToSettingsActivity();
+
 
         }
         if(item.getItemId() == R.id.main_find_friends_option){
 
         }
         return  true;
+    }
+
+    private void SendUserToSettingsActivity() {
+        Intent SettingIntent = new Intent(MainActivity.this,SettingsActivity.class);
+        //we dont want the user to go back to the main activity so added the flags
+        SettingIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(SettingIntent);
+        finish();
     }
 }
--- app/src/main/java/com/example/chatapp/SettingsActivity.java
+++ app/src/main/java/com/example/chatapp/SettingsActivity.java
@@ -3,12 +3,29 @@ package com.example.chatapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private Button updateAccountSettings;
+    private EditText userName, userStatus;
+    private CircleImageView userProfileImage; // the instance of the circle image class added in the dependencies
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        InitializeFields();
+    }
+
+    private void InitializeFields() {
+        updateAccountSettings = (Button) findViewById(R.id.update_settings_button);
+        userName = (EditText) findViewById(R.id.set_user_name);
+        userStatus = (EditText) findViewById(R.id.set_profile_status);
+        userProfileImage = (CircleImageView) findViewById(R.id.profile_image);
     }
 }

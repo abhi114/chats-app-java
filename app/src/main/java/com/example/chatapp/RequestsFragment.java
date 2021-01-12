b936d0fb29ac5a2696d9b6345681a7134132f4c5
abhi114 <nabhi531@gmail.com>
2021-01-12 20:25:15 +0530

    accept and cancel button activated

--- app/src/main/java/com/example/chatapp/RequestsFragment.java
+++ app/src/main/java/com/example/chatapp/RequestsFragment.java
@@ -1,8 +1,10 @@
 package com.example.chatapp;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,9 +14,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -36,7 +41,9 @@ public class RequestsFragment extends Fragment {
     private String currentUserID;
 
     //to display the request users
-    DatabaseReference UsersRef;
+    private  DatabaseReference UsersRef;
+    //to add the profile to contact database after it is accepted
+    private DatabaseReference ContactsRef;
 
 
 
@@ -63,6 +70,8 @@ public class RequestsFragment extends Fragment {
         UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
 
         ChatRequestsRef = FirebaseDatabase.getInstance().getReference().child("Chat Requests");
+
+        ContactsRef = FirebaseDatabase.getInstance().getReference().child("Contacts");
         myRequestsList = (RecyclerView) RequestsFragmentView.findViewById(R.id.chat_requests_list);
         myRequestsList.setLayoutManager(new LinearLayoutManager(getContext()));
 
@@ -93,7 +102,7 @@ public class RequestsFragment extends Fragment {
                 //getting the position of the first sub-category
                 final String list_user_id = getRef(i).getKey();
                 //a reference to the request type
-                DatabaseReference getTypeRef = getRef(i).child("request_type").getRef();
+                final DatabaseReference getTypeRef = getRef(i).child("request_type").getRef();
                 //listener because the value can change and the the requests will automatically update
                 getTypeRef.addValueEventListener(new ValueEventListener() {
                     @Override
@@ -107,23 +116,34 @@ public class RequestsFragment extends Fragment {
                                     public void onDataChange(DataSnapshot dataSnapshot) {
                                         //since image can be optional
                                         if(dataSnapshot.hasChild("image")){
-                                            final String requestUserName = dataSnapshot.child("name").getValue().toString();
-                                            final String requestUserStatus = dataSnapshot.child("status").getValue().toString();
+
                                             final String requestUserProfileImage = dataSnapshot.child("image").getValue().toString();
 
                                             //now display the value
-                                            requestViewHolder.userName.setText(requestUserName);
-                                            requestViewHolder.userStatus.setText(requestUserStatus);
                                             Picasso.get().load(requestUserProfileImage).placeholder(R.drawable.profile_image).into(requestViewHolder.profileImage);
 
 
-                                        }else{
-                                            final String requestUserName = dataSnapshot.child("name").getValue().toString();
-                                            final String requestUserStatus = dataSnapshot.child("status").getValue().toString();
-                                            requestViewHolder.userName.setText(requestUserName);
-                                            requestViewHolder.userStatus.setText(requestUserStatus);
-
                                         }
+                                        final String requestUserName = dataSnapshot.child("name").getValue().toString();
+                                        final String requestUserStatus = dataSnapshot.child("status").getValue().toString();
+                                        requestViewHolder.userName.setText(requestUserName);
+                                        requestViewHolder.userStatus.setText("wants to Connect With You");
+
+                                        requestViewHolder.AcceptButton.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                //options for dialog box
+                                                BuilderShowDialog(list_user_id,currentUserID,requestUserName);
+
+
+                                            }
+                                        });
+                                        requestViewHolder.CancelButton.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                BuilderShowDialog(list_user_id,currentUserID,requestUserName);
+                                            }
+                                        });
 
                                     }
 
@@ -164,6 +184,92 @@ public class RequestsFragment extends Fragment {
         adapter.startListening();
 
 
+    }
+
+    private void BuilderShowDialog(String list_user_id, String currentUserID,String requestUserName) {
+        final String currentUserIDCopy = currentUserID;
+        final String list_user_id_copy = list_user_id;
+        CharSequence options[] = new CharSequence[]{
+                "Accept",
+                "Decline"
+
+        };
+        //alert dialog to confirm the option
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle( requestUserName +" Chat Request");
+        //we had passed the charSequence to the dialog as positive and negative buttons
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if(which == 0){
+                    //it means that the user has clicked on the accept as the index of accept is 0
+                    //remove the user from request and add to contacts fragment
+                    //currentuserid to friend user id and save the value of contacts to saved
+                    ContactsRef.child(currentUserIDCopy).child(list_user_id_copy).child("Contacts").setValue("Saved").addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                //now save for the friend also in the database
+                                ContactsRef.child(list_user_id_copy).child(currentUserIDCopy).child("Contacts").setValue("Saved").addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        //now remove from the request fragment
+                                        if(task.isSuccessful()){
+                                            ChatRequestsRef.child(currentUserIDCopy).child(list_user_id_copy).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    if(task.isSuccessful()){
+                                                        ChatRequestsRef.child(list_user_id_copy).child(currentUserIDCopy).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                if(task.isSuccessful()){
+                                                                    Toast.makeText(getContext(), "Contacts Saved", Toast.LENGTH_SHORT).show();
+                                                                }
+
+                                                            }
+                                                        });
+                                                    }
+
+                                                }
+                                            });
+                                        }
+
+                                    }
+                                });
+                            }
+
+                        }
+                    });
+
+
+                }
+                if(which == 1){
+                    //clicked on cancel
+                    //remove from the request
+                    ChatRequestsRef.child(currentUserIDCopy).child(list_user_id_copy).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                ChatRequestsRef.child(list_user_id_copy).child(currentUserIDCopy).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if(task.isSuccessful()){
+                                            Toast.makeText(getContext(), "Request Canceled", Toast.LENGTH_SHORT).show();
+                                        }
+
+                                    }
+                                });
+                            }
+
+                        }
+                    });
+
+                }
+
+            }
+        });
+
+        builder.show();
     }
 
     public static class RequestViewHolder extends RecyclerView.ViewHolder{
